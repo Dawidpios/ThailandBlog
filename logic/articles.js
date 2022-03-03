60fa2385c57c6e8895e5762b274905dadc3ff971
mk <kosminskimateusz@gmail.com>
2022-03-03 13:42:24 +0100

    Fixed more scroll on article for menu bar space.

--- logic/articles.js
+++ logic/articles.js
@@ -194,7 +194,7 @@ read_more_buttons.forEach((button, id) => {
         current_article.classList.add('slide-down');
 
         article.scrollIntoView();
-        window.scrollBy(0, -70);
+        window.scrollBy(0, -100);
 
       } else if (id !== number) {
         butt.classList.remove('disapeared');
